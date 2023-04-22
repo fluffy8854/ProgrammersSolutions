90e31c43a8a061195134d43dce66801c8ede604d
Lim ji yang <fluffy8854@naver.com>
2023-04-23 00:49:35 +0900

    feat : PrintSS 작성
    \(백슬래시) '(작은 따옴표) "(큰 따옴표)는
    앞에 백슬래시 한개를 붙여서 표시한다.

---
+++ src/main/java/PSolutions/PrintSS.java
@@ -0,0 +1,10 @@
+package PSolutions;
+
+public class PrintSS {
+    public static void main(String[] args) {
+        // \(백슬래시) '(작은 따옴표) "(큰 따옴표)는 앞에 백슬래시 한개를 붙여서 표시한다.
+        // 나머지 특수기호들은 그대로 표시
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+
+    }
+}
